80e0918f5aca3e81e0b3ad3019fa628766b7a811
özkan demir <ozkanndemir2009@gmail.com>
2022-09-17 17:13:49 +0300

    adedd file 5

--- app.js
+++ app.js
@@ -1,77 +1,150 @@
-const text=document.querySelector(".text")
-const btn=document.querySelector(".btn")
-const sonuc=document.querySelector(".sonuc")
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+// const text = document.querySelector(".text");
+// const btn = document.querySelector(".btn");
+// const sonuc = document.querySelector(".sonuc");
+// let sayi;
+// btn.addEventListener("click", () => {
+// if (!text.value) {
+// alert("Lütefen bir kelime giriniz");
+// if (text.value.Match(/[aeiıoöuü]/gi) === null) {
+// sayi = 0;
+// } else {
+// sayi = text.value.Match(/[aeiıoöuü]/gi).length;
+// }
+// // sonuc.innerText = `<p>There are ${text.value.Match(/[aeiıoöuü]/gi).lengt} vovels in <span style="font-size: 1rem;color: red;" >${input.value}</span</p>`;}
+// });
+//
+//
+//  const g=[t,b,c,d]
+// console.log();
+// const text = document.querySelector(".text");
+// const btn = document.querySelector(".btn");
+// const sonuc = document.querySelector(".sonuc");
+// 
+// 
+// 
+// btn.addEventListener("click", () => {
+  // if (!text.value) {
+    // alert("kelime yada cümle giriniz");
+  // }
+  // if (text.value.match(/[aeiıoöuü]/gi) == null) {
+    // sayi = 0;
+  // } else {
+//  let sayi = text.value.match(/[aeiıoöuü]/gi).length;
+    // // sonuc.innerHTML = `<p> <span style="color: black ; font-size:2rem" >There are </span>${sayi} vowels in  <span style="font-size: 2rem; color: red ">${text.value}</span></p>`;
+  //  
+  //  
+  //  
+    // 
+  // }
+// });
 // const text = document.querySelector('.text')
 // const btn = document.querySelector('.btn')
 // const sonuc = document.querySelector('.sonuc')
 // 
-// let sayi;
+// const vowels = ["a", "e", "i", "o", "u"];
 // 
+// function seslisay(text) {
+  // let counter = 0;
+// 
+  // for (let i of text.value.toLowerCase()) {
+    // if (vowels.includes(i)) {
+      // counter++;
+    // }
+  // }
+// 
+  // return counter;
+// }
+// 
+// 
+// function sessizsay(text) {
+  // let counter = 0;
+// 
+  // for (let i of text.value.toLowerCase()) {
+    // if (!vowels.includes(i)) {
+      // counter++;
+    // }
+  // }
+// 
+  // return counter;
+// }
 // btn.addEventListener('click', () => {
   // if (!text.value) {
     // alert('kelime yada cümle giriniz')
-//  else {
-    // if (text.value.match(/[aeiıoöuü]/gi) == null) {
-        // sayi = 0;
-// 
-    // } else{
-      // sayi = text.value.match(/[aeiıoöuü]/gi).length
-// 
-    // }
-    // 
+  // } else {
+    // sonuc.innerHTML = `<p>
+    // ${seslisay(text)} tane sesli , ${sessizsay(text)} tane sessiz harf var
+  //  <span style="font-size: 2rem; color: red ">${input.value}</span>
+//  </p>`;
   // }
-    // }else{
-        // sonuc.innerHTML = `<p>
-        // There are ${text.value.match(/[aeiıoöuü]/gi).length} vowels in
-        // <span style="font-size: 2rem; color: red ">${input.value}</span>
-      //  </p>`;
-        // }
-      // 
-    // }
-  // 
-  // 
-  // 
-  // 
-// 
-  // 
+  // console.log([...text.value]);
 // })
 // 
-// 
-// 
-// 
-// 
+
+// const text = document.querySelector('.text')
+// const btn = document.querySelector('.btn')
+// const sonuc = document.querySelector('.sonuc')
+
+// let sayi;
+
+// btn.addEventListener('click', () => {
+// if (!text.value) {
+// alert('kelime yada cümle giriniz')
+//  {
+// if (text.value.match(/[aeiıoöuü]/gi) == null) {
+// sayi = 0;
+
+// } else{
+// sayi = text.value.match(/[aeiıoöuü]/gi).length
+// console.log(sayi);
+
+// }
+
+// }
+// }else{
+// sonuc.innerHTML = `<p>
+// There are ${text.value.match(/[aeiıoöuü]/gi).length} vowels in
+// <span style="font-size: 2rem; color: red ">${input.value}</span>
+//  </p>`;
+// }
+const text = document.querySelector('.text')
+const btn = document.querySelector('.btn')
+const sonuc = document.querySelector('.sonuc')
+
+const vowels = ["a", "e", "i", "o", "u"];
 
 
+function seslisay(text){
+  let counter=0;
+  for(let i of text.value.toLowerCase()){
+    if(vowels.includes(i)){
+      counter++;
 
 
+    }
+
+  }
+  return counter;
+}
+function sessizsay(text){
+  let counter=0;
+  for(let i of text.value.toLowerCase()){
+    if(!vowels.includes(i)){
+      counter++;
+
+    }
+
+  }
+  return counter;
+
+}
+btn.addEventListener("click",()=>{
+  if(!text.value){
+    alert("lütfen kelime giriniz")
+  }else{
+     sonuc.innerHTML = `<p>
+ ${seslisay(text)} tane sesli , ${sessizsay(text)} tane sessiz harf var
+<span style="font-size: 2rem; color: red ">${input.value}</span><p>`;
+    
+  }
+})
 
